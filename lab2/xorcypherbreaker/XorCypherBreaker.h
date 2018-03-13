36cbf0425d5b0771d42b2911aecf52fe4d055cd2
FlayJalapeno <kornel.prokopowicz@gmail.com>
2018-03-13 23:17:57 +0100

    cipher4

---
+++ lab2/xorcypherbreaker/XorCypherBreaker.h
@@ -0,0 +1,17 @@
+//
+// Created by Kornel on 13.03.2018.
+//
+
+#ifndef JIMP_EXERCISES_XORCYPHERBREAKER_H
+#define JIMP_EXERCISES_XORCYPHERBREAKER_H
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+std::string XorCypherBreaker(const std::vector<char> &cryptogram,
+                             int key_length,
+                             const std::vector<string> &dictionary);
+
+
+#endif //JIMP_EXERCISES_XORCYPHERBREAKER_H
